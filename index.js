8fec991f617aba7f3bbe77b3702ad660a79cf643
Víctor Dual <vdfuste@gmail.com>
2021-02-17 23:57:46 +0100

    Fix delete 'node_modules' from path in windows

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const dirname = __dirname.replace('/node_modules/typemodule', '');
+const dirname = (__dirname.replace('/node_modules/typemodule', '')).replace('\\node_modules\\typemodule', '');
 
 module.exports.__dirname = dirname;
